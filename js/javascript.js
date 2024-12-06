5c0f9e2e731e92af7b1ba4657d02bdc32f5b9c27
nsi-eliott <dragonnedesculs@gmail.com>
2024-12-06 10:47:01 +0100

    js test 1

---
+++ js/javascript.js
@@ -0,0 +1,3 @@
+var vocaloid_spawn = document.getElementsByClassName("vocaloid");
+var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+element.style.opacity = scrollpercent/100;
