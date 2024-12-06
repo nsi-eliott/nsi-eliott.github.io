8c708dd211d7b20de0c7626b35967d08745370bf
nsi-eliott <dragonnedesculs@gmail.com>
2024-12-06 10:51:35 +0100

    test js + bandeau

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,3 @@
 var vocaloid_spawn = document.getElementsByClassName("vocaloid");
 var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-element.style.opacity = scrollpercent/100;
+element.style.opacity = scrollpercent/1000;
